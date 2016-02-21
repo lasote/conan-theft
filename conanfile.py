08d22ce0077cf14783cad7bb662e4567140176c3
Lasote <lasote@gmail.com>
2016-02-21 20:12:19 +0100

    Removed windows, not supported

--- conanfile.py
+++ conanfile.py
@@ -18,6 +18,8 @@ class LibpngConan(ConanFile):
     license="https://github.com/silentbicycle/theft/blob/v0.2.0/LICENSE"
     exports=["CMakeLists.txt"]
     
+    def config(self):
+        self.settings.os.remove("Windows")
        
     def source(self):
         zip_name = "v%s.zip" % self.version
@@ -29,13 +31,19 @@ class LibpngConan(ConanFile):
         """ Define your project building. You decide the way of building it
             to reuse it later in any other project.
         """
-        copyfile("CMakeLists.txt", "%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME)
-        cmake = CMake(self.settings)
-        self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
-        cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
-        self.run('%s && cmake .. %s' % (cd_build, cmake.command_line))
-        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
-            
+        env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
+
+        if self.settings.os == "Linux" or self.settings.os == "Macos":            
+            self.run("cd %s && %s make" % (self.ZIP_FOLDER_NAME, env.command_line))
+#        Windows not compile!!!!
+#         else:
+#             copyfile("CMakeLists.txt", "%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME)
+#             cmake = CMake(self.settings)
+#             self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
+#             cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
+#             self.run('%s && cmake .. %s' % (cd_build, cmake.command_line))
+#             self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+                
     def package(self):
         """ Define your conan structure: headers, libs, bins and data. After building your
             project, this method is called to create a defined structure:
@@ -44,21 +52,13 @@ class LibpngConan(ConanFile):
         self.copy("*.h", "include", "%s" % (self.ZIP_FOLDER_NAME), keep_path=False)
 
         # Copying static and dynamic libs
-        if self.settings.os == "Windows":
-            if self.options.shared:
-                self.copy(pattern="*.dll", dst="bin", src=self.ZIP_FOLDER_NAME, keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-        else:
-            if self.options.shared:
-                if self.settings.os == "Macos":
-                    self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-                else:
-                    self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+        if self.options.shared:
+            if self.settings.os == "Macos":
+                self.copy(pattern="*.dylib", dst="lib", keep_path=False)
             else:
-                self.copy(pattern="*.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+                self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+        else:
+            self.copy(pattern="*.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
 
     def package_info(self):
-        if self.settings.os == "Windows":
-            self.cpp_info.libs = ["theft"]
-        else:
-            self.cpp_info.libs = ["theft"]
+        self.cpp_info.libs = ["theft"]
